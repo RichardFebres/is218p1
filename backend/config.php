42ce4ac6d7b1dc2bd00291717cf13c49e1a1336a
Reybel C <reybel24@gmail.com>
2019-04-15 23:34:21 -0400

    adjust connection settings for NJIT server

--- backend/config.php
+++ backend/config.php
@@ -3,10 +3,10 @@
 // This file defines the database credentials and connection
 
 // Define the database information
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'is218p1');
+define('DB_SERVER', 'sql.njit.edu');
+define('DB_USERNAME', 'rcc442');
+define('DB_PASSWORD', 'mathieu76');
+define('DB_NAME', 'rc442');
 
 // Connect to DB
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
